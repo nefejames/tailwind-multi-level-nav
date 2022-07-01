b7fa2d0cf3717e4d0291accd4260c71237b2ee3d
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2022-07-01 01:10:13 +0100

    cleanup

--- layouts/header/Navbar.js
+++ layouts/header/Navbar.js
@@ -19,15 +19,14 @@ export default function Navbar({ className }) {
             className="inline-flex items-center text-sm xl:text-base text-heading px-3 xl:px-4 py-2 font-normal relative group-hover:text-slate-300"
           >
             {item.label}
-            {(item?.columns && Array.isArray(item.columns)) ||
-              (item.subMenu && (
-                <span className="opacity-30 text-xs mt-1 xl:mt-0.5 w-4 flex justify-end">
-                  <FaChevronDown className="transition duration-300 ease-in-out transform group-hover:-rotate-180" />
-                </span>
-              ))}
+            {item.subMenu && (
+              <span className="opacity-30 text-xs mt-1 xl:mt-0.5 w-4 flex justify-end">
+                <FaChevronDown className="transition duration-300 ease-in-out transform group-hover:-rotate-180" />
+              </span>
+            )}
           </Link>
 
-          {item?.subMenu && Array.isArray(item.subMenu) && (
+          {item?.subMenu && (
             <div className="subMenu shadow-header bg-zinc-900 absolute start-0 opacity-0 group-hover:opacity-100">
               <ul className="text-body text-sm py-5">
                 {item.subMenu.map((menu, i) => {
